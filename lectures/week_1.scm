8bd2b9fa781826b58b4b7cc7b0c087a4948d376e
dsobolev <denis.sobolev1@gmail.com>
2020-05-12 19:42:06 +0300

    Ass.2 Sub-functions localized

--- lectures/week_1.scm
+++ lectures/week_1.scm
@@ -1,20 +1,23 @@
 ; #2 Takes sequence of numbers and returns its squares
 (define (squares l)
-	(build_sequence l '())
-)
 
-(define (sq x) (* x x) )
+	(define (build_sequence inp res)
 
-(define (square_first l) (sq (car l) ) )
+		(set! res (append_squared res inp) ) 
 
-(define (append_squared res l) 
-	(append res (list (square_first l) ) ) 
-)
-
-(define (build_sequence inp res)
-	(set! res (append_squared res inp) ) 
-	(if (eqv? '() (cdr inp) )
-		res 
-		(build_sequence (cdr inp) res)
+		(if (eqv? '() (cdr inp) )
+			res 
+			(build_sequence (cdr inp) res)
+		)
 	)
+
+	(define (append_squared res l) 
+		(append res (list (square_first l) ) ) 
+	)
+
+	(define (square_first l) (sq (car l)))
+
+	(define (sq x) (* x x))
+
+	(build_sequence l '())
 )
