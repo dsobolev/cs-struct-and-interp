bf9435ae493826b717e5134ddb07624f8d837923
dsobolev <denis.sobolev1@gmail.com>
2020-05-16 00:30:01 +0300

    Ex. 1.29 Define sum_set. Ready

--- book/chapter_1_3.scm
+++ book/chapter_1_3.scm
@@ -1,30 +1,49 @@
 ; Common sum procedure
 (define (sum term a next b)
-	(if (> a b)
-		0
-		(+ (term a) 
-		   (sum term (next a) next b))
-	)
+    (if (> a b)
+        0
+        (+ (term a) 
+           (sum term (next a) next b))
+    )
 )
 
 ; Ex. 1.29 Simpson's Rule of numerical integration
 ; f - the actual function. cube us used for testing
 ; a, b - boundaries of an area
 ; n - number of iterations, should be even
-(define (simpsons_integral f, a, b, n)
-	(if (odd? n) (set! n (+ n 1)))
+(define (simpsons_integral f a b n)
+    (if (odd? n) (set! n (+ n 1)))
 
-	(define (сoef i)
-		(if (or (= 0 i) (= n i)) 
-			1
-			(if (even? i) 
-				2
-				4
-			)
-		)
-	)
+    (define h (/ (- b a) n))
 
-	(define term)
+    (define (coef i)
+        (if (or (= 0 i) (= n i)) 
+            1
+            (if (even? i) 
+                2
+                4
+            )
+        )
+    )
+
+    (define (arg_val i) (+ a (* i h)))
+
+    (define (sum_set i)
+        (if (> i n)
+            0
+            (+ (* (coef i) 
+                  (f (arg_val i)) ) 
+               (sum_set (+ i 1))
+            )
+        )
+    )
+
+    (* (/ h 3)
+       (sum_set 0))
 )
 
 (define (cube x) (* x x x))
+
+; To check simpsons_integral
+(simpsons_integral cube 0 1 100)
+(simpsons_integral cube 0 1 1000)
